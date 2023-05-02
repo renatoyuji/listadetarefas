0536466c263676963312e949391b38ec3bd59ea9
renato <renatotanaka2002@gmail.com>
2023-05-01 21:23:30 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
-    $('.botao').on('submit', function(e){
+    $('form').on('submit', function(e){
         e.preventDefault();
 
-        const tarefa = $('#tarefa').val();
-        const linha = document.createElement('li')
-        li.innerHTML = `${tarefa}`;
-        $('ul').append(linha)
+        const nomeTarefa = $('#tarefa').val();
+        const novaTarefa = $('<li></li>');
+        $(`<p>${nomeTarefa}</p>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
         
         $('#tarefa').val("");
 
-    })
+        $('li').click(function(){
+            $(this).addClass('riscado');
+        })
+})
 })
